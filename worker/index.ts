8a8ff3b65e654d96891bb3b011d4627acb757bf4
loto09090909-lab <loto09090909@gmail.com>
2025-12-15 13:17:25 +0900

    Update index.ts

--- worker/index.ts
+++ worker/index.ts
@@ -115,3 +115,27 @@ function jsResponse() {
     headers: { "Content-Type": "application/javascript; charset=UTF-8" },
   });
 }
+
+function json(data: any, status = 200) {
+  return new Response(JSON.stringify(data), {
+    status,
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET,POST,OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type"
+    }
+  });
+}
+
+function json(data: any, status = 200) {
+  return new Response(JSON.stringify(data), {
+    status,
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET,POST,OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type"
+    }
+  });
+}
